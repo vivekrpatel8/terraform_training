857187f9b0b3b1f2ad559bf1c1ea371bf5e81700
vivek <vivek@atvenu.com>
2026-09-21 16:14:51 -0600

    chore test

--- project_1/variables.tf
+++ project_1/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   type        = string
-  description = "AWS region for all resourcesssss"
+  description = "AWS region for all resourcessssss"
   default     = "us-east-1"
 }
