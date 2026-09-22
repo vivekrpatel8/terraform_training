e912b53a84f6ad7f893671820f70550d6b18f05c
vivek <vivek@atvenu.com>
2026-09-21 18:54:45 -0600

    test steps

--- broken_project/main.tf
+++ broken_project/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.2"
 }
 
-# variable "example" {
-#   type        = string
-#   description = "Example input variable."
-#   default     = "value"
-# }
+variable "example" {
+  type        = string
+  description = "Example input variable."
+  default     = "value"
+}
 
 output "example" {
   value = var.example
